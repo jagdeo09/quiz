48341196d5d43762c2950cdc20a26d4f242ca39a
Petal Jagdeo <petaljagdeo1@gmail.com>
2019-02-14 22:02:20 +0000

    finished

--- js/script.js
+++ js/script.js
@@ -19,15 +19,15 @@ $(document).ready(function() {
     });
         function findQues1(q1Result){
             if (q1Result === "red"){
-                return 2;
+                return 7;
             }
             else if (q1Result === "blue"){
-                return 3
+                return 5;
             }
         }
         function findQues2(q2Result){
             if (q2Result === "burger"){
-                return 3;
+                return 8;
             }
             else if (q2Result === "pizza"){
                 return 4;
@@ -35,7 +35,7 @@ $(document).ready(function() {
         }
         function findQues3(q3Result){
             if (q3Result === "the avengers"){
-                return 3;
+                return 9;
             }
             else if (q3Result === "creed"){
                 return 5;
@@ -43,10 +43,10 @@ $(document).ready(function() {
         }
         function findQues4(q4Result){
             if (q4Result === "tattoo"){
-                return 4;
+                return 3;
             }
             else if (q4Result === "eyes closed"){
-                return 2;
+                return 6;
             }
         }
      function placement(totalScore, name) {
@@ -54,28 +54,30 @@ $(document).ready(function() {
         var imgSrc;
         if (totalScore > 20) {
             techplacement = "Brady Tutton";
-            imgSrc="https://media.giphy.com/media/oeH2Qj4fLoZy0/giphy.gif";
+            imgSrc="https://m.media-amazon.com/images/M/MV5BMjNkYzg5NGItYzA2Yi00ODA1LTk5NGItMzEwYzdhMDViZjQ5XkEyXkFqcGdeQXVyMjM3NTI1Nzg@._V1_SY1000_CR0,0,666,1000_AL_.jpg";
         }
         else if (totalScore >16 && totalScore <= 20) {
             techplacement = "Chance Perez";
-            imgSrc="https://media.giphy.com/media/l41lX2yEwhnD6QrLi/giphy.gif";
+            imgSrc="https://pbs.twimg.com/media/DBQDIEUV0AEbaNS.jpg";
         }
         else if (totalScore > 11 && totalScore <= 16) {
             techplacement = "Michael Conor";
-            imgSrc="https://media.giphy.com/media/zWyv9xtsEfXQQ/giphy.gif";
+            imgSrc="https://pbs.twimg.com/media/DICyAvXVYAAK24z.jpg";
         }
         else if (totalScore > 8 && totalScore <= 11) {
             techplacement = "Drew Ramos";
-            imgSrc="https://media.giphy.com/media/RUvPszdoBXWIU/giphy.gif";
+            imgSrc="https://cdn1.edgedatg.com/aws/v2/abc/BoyBand/person/2048461/133031f8182422898ce2047c6b1fa78d/320x180-Q90_133031f8182422898ce2047c6b1fa78d.jpg";
         }
-         else if (totalScore == 8) {
+         else if (totalScore <= 8) {
             techplacement = "Sergio Calderon";
-            imgSrc="https://media.giphy.com/media/xUPGcmrdRkCaZ5qZ2M/giphy.gif";
+            imgSrc="https://cdn1.edgedatg.com/aws/v2/abc/BoyBand/person/2048472/6661dff68891e0878a442d09b92a7365/320x180-Q90_6661dff68891e0878a442d09b92a7365.jpg";
         }
         displayPlacement(name, techplacement, imgSrc);
     }
-    function displayPlacement(name, person, image){
-        $(".result").html("<div>Hi</div>");
+    function displayPlacement(name, person, imgSrc){
+        $(".result").html("<h1>Congrats " + name + " you end up with " + person + "<img src= " + imgSrc + ">" + "</h1>");
+        $(".result").css("height", "20%");
+        $(".result").css("width", "20%");
     }
 
 
